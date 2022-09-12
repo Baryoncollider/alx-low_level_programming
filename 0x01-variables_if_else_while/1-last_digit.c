b5bd86f2b181d2959eacd3d4401e31c3f7ca3814
Baryoncollider <isahmohammedisiaka@gmail.com>
2022-09-12 08:59:12 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,11 @@ int main(void)
 {
 int n;
 int 1;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 1 = n % 10;
+
 if (1 > 5)
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, 1);
@@ -29,5 +31,3 @@ else
 }
 return (0);
 }
-
-
