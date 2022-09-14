56869f2920952dff901b1fe38d0bdf23fc88364d
Baryoncollider <isahmohammedisiaka@gmail.com>
2022-09-14 09:21:10 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign -> prints sign based on condition
+ * print_sign - function that prints the sign of a number
  * @n: argument passed
- * Return: 1, 0 -1
+ *
+ * Return: return 1, 0 or -1 as the case may be
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -15,11 +17,11 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		return  (0);
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
