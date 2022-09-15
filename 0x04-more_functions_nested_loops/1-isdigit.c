9714184308bbff341736c5136a46275f0836062b
Baryoncollider <isahmohammedisiaka@gmail.com>
2022-09-15 07:32:39 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * _isdigit - function to check a digit
+ * @c: an integer argument
+ *
+ * Return: Success
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
