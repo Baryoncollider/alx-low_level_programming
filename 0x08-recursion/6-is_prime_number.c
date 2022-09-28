6778d25795cbb1470a95bf1d235fb0617a219b4d
Baryoncollider <isahmohammedisiaka@gmail.com>
2022-09-28 14:33:58 -0700

    hot fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,9 +24,9 @@ int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-	if (n % 1 == 0 && i > 1)
+	if (n % i == 0 && i > 1)
 		return (0);
 	if ((n / i) < i)
 		return (1);
-	return (check_prime(n, 1 + 1));
+	return (check_prime(n, i + 1));
 }
